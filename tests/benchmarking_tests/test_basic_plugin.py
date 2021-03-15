4be4219a9551cf39765632494bc6a38f5c048c39
Eoghan O'Connell <eoclives@hotmail.com>
2021-03-15 16:26:07 +0100

    github-actions: added a benchmarking step, initial check

--- tests/benchmarking_tests/test_basic_plugin.py
+++ tests/benchmarking_tests/test_basic_plugin.py
@@ -4,7 +4,3 @@ from tests import test_basic
 
 def test_benchmark_simulator(benchmark):
     benchmark(test_basic.test_run_plugin_with_simulator)
-
-# def test_benchmark_simulator_ped(benchmark):
-#     benchmark.pedantic(test_basic.test_run_plugin_with_simulator,
-#                        iterations=5, rounds=5)
